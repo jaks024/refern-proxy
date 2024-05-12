cb9e0b2f0c0ce60bc6f26d02c9ac73c8480402ce
jaks024 <42505518+jaks024@users.noreply.github.com>
2024-05-11 23:49:08 -0400

    added console logs

--- src/index.js
+++ src/index.js
@@ -9,15 +9,23 @@ proxyServer = proxy.createProxyServer({
     host: process.env.PROXY_HOST_URL,
 });
 
+console.log('host', process.env.PROXY_HOST_URL)
+console.log('mirror', process.env.PROXY_MIRROR_URL)
+console.log('client', process.env.PROXY_CLIENT_URL)
+
 const app = express();
 const port = process.env.PORT;
 
 app.use('/', async (req, res, next) => {
+    console.log('request', req)
+    console.log('user agent', req.headers['user-agent'])
     if (isbot(req.headers['user-agent'])) {
+        console.log('is bot')
         proxyServer.web(req, res, {
             target: process.env.PROXY_MIRROR_URL,
         }, next);
     } else {
+        console.log('is not bot')
         proxyServer.web(req, res, {
             target: process.env.PROXY_CLIENT_URL
         }, next);
