438203fe930da3e13f20351d9290f8971a2c2fed
jaks024 <42505518+jaks024@users.noreply.github.com>
2024-05-11 21:01:03 -0400

    added base proxy

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const  dotenv = require('dotenv');
+const express = require('express');
+const { isbot } = require('isbot');
+const proxy = require('http-proxy');
+
+dotenv.config({ path: './.env.development.local' });
+
+proxyServer = proxy.createProxyServer({
+    host: process.env.PROXY_HOST_URL,
+});
+
+const app = express();
+const port = process.env.PORT;
+
+app.use('/', async (req, res, next) => {
+    if (isbot(req.headers['user-agent'])) {
+        proxyServer.web(req, res, {
+            target: process.env.PROXY_MIRROR_URL,
+        }, next);
+    } else {
+        proxyServer.web(req, res, {
+            target: process.env.PROXY_CLIENT_URL
+        }, next);
+    }
+});
+
+
+app.listen(port, () => {
+  console.log(`refern proxy listening on port ${port}`)
+})
